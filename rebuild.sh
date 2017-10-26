07dadd78733921b8b32255669b5d22bd06673e3e
marvinfy <marvin@marvinfy.com>
2017-10-26 19:37:15 -0230

    Made rebuild script smarter

--- rebuild.sh
+++ rebuild.sh
@@ -1,1 +1,14 @@
-rm -r ./build && truffle compile && truffle migrate
+if [ -d "./build" ]; then
+  rm -rf ./build
+fi
+
+truffle compile
+
+if [[ $? -eq 0 ]]; then
+  truffle migrate
+
+  if [[ $? -eq 0 ]]; then
+    truffle test
+  fi
+  
+fi
