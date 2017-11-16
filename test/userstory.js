62da9c616071a004ecd32fa0d1f69c3611fc6f5f
marvinfy <marvin@marvinfy.com>
2017-11-15 23:44:35 -0330

    Added userstory test file

---
+++ test/userstory.js
@@ -0,0 +1,10 @@
+var PatientFactory = artifacts.require("./PatientFactory.sol");
+var Patient = artifacts.require("./Patient.sol");
+
+contract('All (User Story)', function(accounts) {
+  it("should assert true", function(done) {
+    //var userstory = userstory.deployed();
+    assert.isTrue(true);
+    done();
+  });
+});
